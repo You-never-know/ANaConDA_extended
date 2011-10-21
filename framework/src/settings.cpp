a29a5d9b1ac505a4fe8b2c2ca32e7748e7a157b3
Jan Fiedor <fiedorjan@centrum.cz>
2011-10-21 16:08:11 +0200

    framework/settings: added support for loading exclusion patterns.
    The loadEnvVars() method now uses the getenv() function to retrieve the
    values of environment variables, because the environ variable sometimes
    contain malformed environment variable values. If the loaded patterns
    contain references to some environment variables, the references are
    replaced by the values of these variables.

--- framework/src/settings.cpp
+++ framework/src/settings.cpp
@@ -8,8 +8,8 @@
  * @file      settings.cpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2011-10-20
- * @date      Last Update 2011-10-20
- * @version   0.1.1
+ * @date      Last Update 2011-10-21
+ * @version   0.1.2
  */
 
 #include "settings.h"
@@ -18,6 +18,9 @@
   #include <boost/tokenizer.hpp>
 #endif
 
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
 /**
  * Loads the ANaConDA framework settings.
  */
@@ -25,6 +28,9 @@ void Settings::load()
 {
   // Load environment variables (might be referenced later)
   this->loadEnvVars();
+
+  // Load patterns describing excluded images
+  this->loadExclusions();
 }
 
 /**
@@ -36,14 +42,29 @@ void Settings::load()
 void Settings::print(std::ostream& s)
 {
   // Helper variables
-  EnvVarMap::iterator it;
+  EnvVarMap::iterator envIt;
+  PatternList::iterator pIt;
+
+  // Print the ANaConDA framework settings
+  s << "Settings\n"
+    << "--------\n";
 
   // Print a section containing loaded environment variables
-  s << "Environment variables\n---------------------\n";
+  s << "\nEnvironment variables"
+    << "\n---------------------\n";
 
-  for (it = m_env.begin(); it != m_env.end(); it++)
+  for (envIt = m_env.begin(); envIt != m_env.end(); envIt++)
   { // Print each environment variable
-    s << it->first << "=" << it->second << std::endl;
+    s << envIt->first << "=" << envIt->second << std::endl;
+  }
+
+  // Print a section containing loaded instrumentation exclusion patterns
+  s << "\nImages which will not be instrumented"
+    << "\n-------------------------------------\n";
+
+  for (pIt = m_insExclusions.begin(); pIt != m_insExclusions.end(); pIt++)
+  { // Print each instrumentation exclusion pattern
+    s << *pIt << std::endl;
   }
 }
 
@@ -54,7 +75,7 @@ void Settings::loadEnvVars()
 {
 #ifdef TARGET_LINUX
   // Helper variables
-  std::string name, value;
+  std::string name;
 
   // On POSIX systems, all environment variables are stored in this variable
   extern char **environ;
@@ -69,16 +90,109 @@ void Settings::loadEnvVars()
   { // Split each environment variable to 'name' and 'value' pairs
     tokenizer tokens(std::string(environ[i]), sep);
 
-    // Access the parts of the environment variable
-    tokenizer::iterator it = tokens.begin();
+    // The first token must be the name of the environment variable
+    name = *tokens.begin();
 
-    name = *it++; // First part is the name of the environment variable
-    value = *it; // Second part is the value of the environment variable
-
-    // Save the environment variable to the settings object
-    m_env[name] = value;
+    // Use 'getenv' to get the value, 'environ' might have the values malformed
+    m_env[name] = getenv(name.c_str());
   }
 #endif
+}
+
+/**
+ * Loads patterns describing images (executables, shared objects, dynamic
+ *   libraries, ...) which should be excluded from instrumentation and/or
+ *   debugging information extraction.
+ */
+void Settings::loadExclusions()
+{
+  // The framework presumes that configuration files are in the 'conf' directory
+  boost::filesystem::path confDir = boost::filesystem::current_path() /= "conf";
+
+  // Images excluded from instrumentation are specified in 'ins-excludes' file
+  boost::filesystem::path insExcFile = confDir /= "ins-excludes";
+
+  if (boost::filesystem::exists(insExcFile))
+  { // Extract all instrumentation exclusion patterns
+    boost::filesystem::fstream f(insExcFile);
+
+    // Helper variables
+    std::string line;
+
+    while (std::getline(f, line) && !f.fail())
+    { // Each line of the file contain one exclusion pattern
+      m_insExclusions.push_back(this->expandEnvVars(line));
+    }
+  }
+}
+
+/**
+ * Expands all environment variables in a string.
+ *
+ * @param s A string containing references to one or more environment variables.
+ * @return A string with all environment variables replaced by their values.
+ */
+std::string Settings::expandEnvVars(std::string s)
+{
+  // Helper variables
+  std::string expanded;
+  std::string::iterator it = s.begin();
+
+  while (it != s.end())
+  { // Search the whole string for references to environment variables
+    if (*it == '$')
+    { // An environment variable specification or the '$' character must follow
+      if (*++it == '$')
+      { // Escaped '$' character, insert it to string and move to the next char
+        expanded += *it;
+      }
+      else if (*it == '{')
+      { // Beginning of the specification of a name of an environment variable
+        std::string name;
+
+        while (++it != s.end())
+        { // Get the name of the environment variable
+          if (*it == '}')
+          { // Valid name specification
+            break;
+          }
+          else
+          { // Part of the name specification
+            name += *it;
+          }
+        }
+
+        if (it == s.end())
+        { // Name specification incomplete, for now ignore and keep the text
+          return expanded + "${" + name;
+        }
+
+        // Helper variables
+        EnvVarMap::iterator envIt;
+
+        if ((envIt = m_env.find(name)) != m_env.end())
+        { // Referenced existing environment variable, replace it with its value
+          expanded += envIt->second;
+        }
+        else
+        { // Referenced non-existent environment variable, cannot expand it
+          expanded += "${" + name + "}";
+        }
+      }
+      else
+      { // Invalid character after '$', for now ignore and keep the text
+        expanded += "$" + *it;
+      }
+    }
+    else
+    { // Other characters should just be copied to the expanded string
+      expanded += *it;
+    }
+
+    it++; // Move to the next character in the string
+  }
+
+  return expanded; // Return the string with expanded environment variables
 }
 
 /** End of file settings.cpp **/
--- framework/src/settings.h
+++ framework/src/settings.h
@@ -7,18 +7,20 @@
  * @file      settings.h
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2011-10-20
- * @date      Last Update 2011-10-20
- * @version   0.1.1
+ * @date      Last Update 2011-10-21
+ * @version   0.1.2
  */
 
 #ifndef __PINTOOL_ANACONDA__SETTINGS_H__
   #define __PINTOOL_ANACONDA__SETTINGS_H__
 
 #include <iostream>
+#include <list>
 #include <map>
 
 // Type definitions
 typedef std::map< std::string, std::string > EnvVarMap;
+typedef std::list< std::string > PatternList;
 
 /**
  * @brief A class holding the ANaConDA framework settings.
@@ -27,18 +29,26 @@ typedef std::map< std::string, std::string > EnvVarMap;
  *
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2011-10-20
- * @date      Last Update 2011-10-20
- * @version   0.1
+ * @date      Last Update 2011-10-21
+ * @version   0.1.1
  */
 class Settings
 {
   private: // Retrieved variables
     EnvVarMap m_env; //!< A map containing values of environment variables.
+    /**
+     * @brief A list containing patterns describing images which should not be
+     *   instrumented.
+     */
+    PatternList m_insExclusions;
   public: // Member methods for handling the ANaConDA framework settings
     void load();
     void print(std::ostream& s = std::cout);
-  private: // Internal helper methods
+  private: // Internal helper methods for loading parts of the settings
     void loadEnvVars();
+    void loadExclusions();
+  private: // Internal helper methods
+    std::string expandEnvVars(std::string s);
 };
 
 #endif /* __PINTOOL_ANACONDA__SETTINGS_H__ */
