706181a7dd3f0f82d664adcb6c02bdc07057322a
Jan Fiedor <fiedorjan@centrum.cz>
2012-01-27 11:22:48 +0100

    event-printer: now prints information about before memory read events.

--- analysers/event-printer/src/event-printer.cpp
+++ analysers/event-printer/src/event-printer.cpp
@@ -6,13 +6,44 @@
  * @file      event-printer.cpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2012-01-05
- * @date      Last Update 2012-01-16
- * @version   0.1.1.1
+ * @date      Last Update 2012-01-27
+ * @version   0.1.2
  */
 
-#include <iostream>
-
 #include "anaconda.h"
+
+/**
+ * Gets a declaration of a variable.
+ *
+ * @param variable A structure containing the information about the variable.
+ * @return A string containing the declaration of the variable.
+ */
+inline
+std::string getVariableDeclaration(const VARIABLE& variable)
+{
+  // Format the name, type and offset to a 'type name[+offset]' string
+  return ((variable.type.size() == 0) ? "" : variable.type + " ")
+    + ((variable.name.empty()) ? "<unknown>" : variable.name)
+    + ((variable.offset == 0) ? "" : "+" + decstr(variable.offset));
+}
+
+/**
+ * Prints information about a read from a memory.
+ *
+ * @param tid A thread which performed the read.
+ * @param addr An address from which were the data read.
+ * @param size A size in bytes of the data read.
+ * @param variable A structure containing information about a variable stored
+ *   at the address from which were the data read.
+ */
+VOID beforeMemoryRead(THREADID tid, ADDRINT addr, UINT32 size,
+  const VARIABLE& variable)
+{
+  CONSOLE("Thread " + decstr(tid)
+    + " read " + decstr(size) + " " + ((size == 1) ? "byte" : "bytes")
+    + " from " + getVariableDeclaration(variable)
+    + " [address " + hexstr(addr) + "]\n");
+}
 
 /**
  * Prints information about a lock acquisition.
@@ -116,6 +147,9 @@ VOID afterWait(THREADID tid, COND cond)
 extern "C"
 void init()
 {
+  // Register callback functions called before access events
+  ACCESS_BeforeMemoryRead(beforeMemoryRead);
+
   // Register callback functions called before synchronisation events
   SYNC_BeforeLockAcquire(beforeLockAcquire);
   SYNC_BeforeLockRelease(beforeLockRelease);
