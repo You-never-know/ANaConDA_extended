4a32da9d74727cae5bfb7038d8bf1f721baa2d69
Jan Fiedor <fiedorjan@centrum.cz>
2012-03-09 11:07:36 +0100

    Added the GoodLock deadlock detector.

---
+++ analysers/goodlock/src/goodlock.cpp
@@ -0,0 +1,24 @@
+/**
+ * @brief Contains the entry part of the GoodLock ANaConDA plugin.
+ *
+ * A file containing the entry part of the GoodLock ANaConDA plugin.
+ *
+ * @file      goodlock.cpp
+ * @author    Jan Fiedor (fiedorjan@centrum.cz)
+ * @date      Created 2012-03-09
+ * @date      Last Update 2012-03-09
+ * @version   0.1
+ */
+
+#include "anaconda.h"
+
+/**
+ * Initialises the GoodLock plugin.
+ */
+extern "C"
+void init()
+{
+  //
+}
+
+/** End of file goodlock.cpp **/
