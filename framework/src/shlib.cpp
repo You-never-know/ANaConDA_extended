fc31330dfce5b72c7783da4528b4763b25086ea7
Jan Fiedor <fiedorjan@centrum.cz>
2012-06-01 10:51:27 +0200

    framework/shlib: ported to Windows, moved internal data definitions to source.
    Internal data are now accessed through an opaque pointer so no platform
    specific data types are now present in the header file. This solves the
    problem of name clashing between the Windows and PIN header files.

--- framework/src/shlib.cpp
+++ framework/src/shlib.cpp
@@ -6,28 +6,51 @@
  * @file      shlib.cpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2011-12-08
- * @date      Last Update 2012-01-21
- * @version   0.1.1.1
+ * @date      Last Update 2012-06-01
+ * @version   0.2
  */
 
 #include "shlib.h"
 
 #include <assert.h>
 
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
+#ifdef TARGET_WINDOWS
   #include <windows.h>
+
+  #include <boost/lexical_cast.hpp>
 #else
   #include "linux/dlutils.h"
 #endif
 
+#ifdef TARGET_WINDOWS
+  typedef HMODULE shlib_handle_t;
+#else
+  typedef void* shlib_handle_t;
+#endif
+
+/**
+ * @brief A structure holding internal data.
+ */
+struct SharedLibrary::Data
+{
+  fs::path path; //!< A path to a shared library (.dll or .so file).
+  shlib_handle_t handle; //!< A handle representing a shared library.
+
+  /**
+   * Constructs a Data object.
+   *
+   * @param p A path to a shared library (.dll or .so file).
+   * @param h A handle representing a shared library.
+   */
+  Data(fs::path p, shlib_handle_t h) : path(p), handle(h) {}
+};
+
 /**
  * Constructs a SharedLibrary object.
  *
- * @param path A path to a shared library (.dll or .so file).
- * @param handle A handle representing a shared library.
+ * @param data A structure holding internal data.
  */
-SharedLibrary::SharedLibrary(fs::path path, shlib_handle_t handle)
-  : m_path(path), m_handle(handle)
+SharedLibrary::SharedLibrary(Data* data) : m_data(data)
 {
 }
 
@@ -36,19 +59,21 @@ SharedLibrary::SharedLibrary(fs::path path, shlib_handle_t handle)
  *
  * @param sl An object representing a shared library.
  */
-SharedLibrary::SharedLibrary(const SharedLibrary& sl) : m_path(sl.m_path)
+SharedLibrary::SharedLibrary(const SharedLibrary& sl)
 {
   // To close a shared library, one must delete the object representing it, so
   // we are always copying an opened shared library here, opening the library
   // again will only increment the library handle reference count, so no error
   // should occur here (everything is already loaded), but we are closing the
   // library when deleting the object, so we must open it during construction
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-  m_handle = LoadLibrary(m_path.native().c_str());
+  m_data.reset(new Data(sl.m_data->path,
+#ifdef TARGET_WINDOWS
+    LoadLibrary(sl.m_data->path.string().c_str())));
 #else
-  m_handle = dlopen(m_path.native().c_str(), RTLD_LAZY|RTLD_GLOBAL);
+    dlopen(sl.m_data->path.string().c_str(), RTLD_LAZY|RTLD_GLOBAL)));
 #endif
-  assert(m_handle == sl.m_handle); // Must be the same library which was copied
+  // Must be the same library which was copied
+  assert(m_data->handle == sl.m_data->handle);
 }
 
 /**
@@ -59,10 +84,10 @@ SharedLibrary::~SharedLibrary()
   // Close the shared library, if the library's handle reference count is zero,
   // e.g., no other object representing this particular library is present, the
   // library will be unloaded from the memory of the process
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-  FreeLibrary(m_handle);
+#ifdef TARGET_WINDOWS
+  FreeLibrary(m_data->handle);
 #else
-  dlclose(m_handle);
+  dlclose(m_data->handle);
 #endif
 }
 
@@ -77,25 +102,25 @@ SharedLibrary::~SharedLibrary()
  */
 SharedLibrary* SharedLibrary::Load(fs::path path, std::string& error)
 {
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-  shlib_handle_t handle = LoadLibrary(path.native().c_str());
+#ifdef TARGET_WINDOWS
+  shlib_handle_t handle = LoadLibrary(path.string().c_str());
 #else
-  shlib_handle_t handle = dlopen(path.native().c_str(), RTLD_LAZY|RTLD_GLOBAL);
+  shlib_handle_t handle = dlopen(path.string().c_str(), RTLD_LAZY|RTLD_GLOBAL);
 #endif
 
   if (handle == NULL)
   { // Shared library could not be loaded
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-    error = "LoadLibrary(" + path.native() + ") failed (error code "
+#ifdef TARGET_WINDOWS
+    error = "LoadLibrary(" + path.string() + ") failed (error code "
       + boost::lexical_cast< std::string >(::GetLastError()) + ").";
 #else
-    error = "dlopen(" + path.native() + ") failed (" + dlerror() + ").";
+    error = "dlopen(" + path.string() + ") failed (" + dlerror() + ").";
 #endif
     return NULL;
   }
 
   // Only here can be created a new shared library object (private constructor)
-  return new SharedLibrary(path, handle);
+  return new SharedLibrary(new Data(path, handle));
 }
 
 /**
@@ -107,11 +132,21 @@ SharedLibrary* SharedLibrary::Load(fs::path path, std::string& error)
  */
 void* SharedLibrary::resolve(const std::string& symbol)
 {
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-  return GetProcAddress(m_handle, symbol.c_str());
+#ifdef TARGET_WINDOWS
+  return GetProcAddress(m_data->handle, symbol.c_str());
 #else
-  return dlsym(m_handle, symbol.c_str());
+  return dlsym(m_data->handle, symbol.c_str());
 #endif
+}
+
+/**
+ * Gets a path to a shared library.
+ *
+ * @return The path to the shared library.
+ */
+const fs::path& SharedLibrary::getPath()
+{
+  return m_data->path;
 }
 
 /**
@@ -122,11 +157,12 @@ void* SharedLibrary::resolve(const std::string& symbol)
  */
 void* SharedLibrary::getAddress()
 {
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-  // TODO: use LoadLibrary
+#ifdef TARGET_WINDOWS
+  // The value of the handle is the address at which is the library loaded
+  return (void*)m_data->handle;
 #else
   // The shared library must be loaded here, so the address must be known
-  return (void*)dl_get_sobj(absolute(this->m_path).c_str()).dlsi_addr;
+  return (void*)dl_get_sobj(absolute(m_data->path).c_str()).dlsi_addr;
 #endif
 }
 
--- framework/src/shlib.h
+++ framework/src/shlib.h
@@ -6,12 +6,16 @@
  * @file      shlib.h
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2011-12-08
- * @date      Last Update 2012-01-14
- * @version   0.1.1.1
+ * @date      Last Update 2012-06-01
+ * @version   0.2
  */
 
 #ifndef __PINTOOL_ANACONDA__SHLIB_H__
   #define __PINTOOL_ANACONDA__SHLIB_H__
+
+#ifdef TARGET_WINDOWS
+  #include <memory>
+#endif
 
 #include <boost/filesystem.hpp>
 
@@ -19,13 +23,9 @@
 namespace fs = boost::filesystem;
 
 // Type definitions
-#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN__)
-  typedef HMODULE shlib_handle_t;
-
+#ifdef TARGET_WINDOWS
   #define SHLIB_EXT ".dll"
 #else
-  typedef void* shlib_handle_t;
-
   #define SHLIB_EXT ".so"
 #endif
 
@@ -37,19 +37,19 @@ namespace fs = boost::filesystem;
  *
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2011-12-08
- * @date      Last Update 2012-01-06
- * @version   0.1.1
+ * @date      Last Update 2012-06-01
+ * @version   0.2
  */
 class SharedLibrary
 {
-  private: // User-defined variables
-    fs::path m_path; //!< A path to a shared library (.dll or .so file).
+  private: // Declarations of opaque types
+    struct Data;
   private: // Internal variables
-    shlib_handle_t m_handle; //!< A handle representing a shared library.
+    std::unique_ptr< Data > m_data; //!< A structure holding internal data.
   public: // Static methods
     static SharedLibrary* Load(fs::path path, std::string& error);
   private: // Internal constructors
-    SharedLibrary(fs::path path, shlib_handle_t handle);
+    SharedLibrary(Data* data);
   public: // Constructors
     SharedLibrary(const SharedLibrary& sl);
   public: // Destructors
@@ -57,17 +57,8 @@ class SharedLibrary
   public: // Member methods
     void* resolve(const std::string& symbol);
   public: // Member methods for retrieving information about the shared library
+    const fs::path& getPath();
     void* getAddress();
-  public: // Inline member methods
-    /**
-     * Gets a path to a shared library.
-     *
-     * @return The path to the shared library.
-     */
-    const fs::path& getPath()
-    {
-      return m_path;
-    }
 };
 
 #endif /* __PINTOOL_ANACONDA__SHLIB_H__ */
