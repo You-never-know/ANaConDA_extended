be41cfa04384457941ef30d0d744cc9511bbdda5
Jan Fiedor <fiedorjan@centrum.cz>
2012-06-22 16:24:39 +0200

    framework/tls: API functions are now properly exported.

--- framework/src/pin/tls.h
+++ framework/src/pin/tls.h
@@ -7,8 +7,8 @@
  * @file      tls.h
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2012-02-04
- * @date      Last Update 2012-02-04
- * @version   0.1
+ * @date      Last Update 2012-06-22
+ * @version   0.1.0.1
  */
 
 #ifndef __PINTOOL_ANACONDA__PIN__TLS_H__
@@ -16,9 +16,11 @@
 
 #include "pin.H"
 
-TLS_KEY TLS_CreateThreadDataKey(DESTRUCTFUN dfunc);
-VOID* TLS_GetThreadData(TLS_KEY key, THREADID tid);
-BOOL TLS_SetThreadData(TLS_KEY key, const VOID* data, THREADID tid);
+#include "../defs.h"
+
+API_FUNCTION TLS_KEY TLS_CreateThreadDataKey(DESTRUCTFUN dfunc);
+API_FUNCTION VOID* TLS_GetThreadData(TLS_KEY key, THREADID tid);
+API_FUNCTION BOOL TLS_SetThreadData(TLS_KEY key, const VOID* data, THREADID tid);
 
 #endif /* __PINTOOL_ANACONDA__PIN__TLS_H__ */
 
