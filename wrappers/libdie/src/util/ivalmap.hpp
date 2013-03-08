2556c995eb5905ce86a82197dfa0ae8248508f92
Jan Fiedor <fiedorjan@centrum.cz>
2013-03-08 13:06:51 +0100

    libdie-wrapper/ivalmap: added iterator type and end() method.

--- wrappers/libdie/src/util/ivalmap.hpp
+++ wrappers/libdie/src/util/ivalmap.hpp
@@ -6,8 +6,8 @@
  * @file      ivalmap.hpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2013-03-07
- * @date      Last Update 2013-03-07
- * @version   0.1
+ * @date      Last Update 2013-03-08
+ * @version   0.2
  */
 
 #ifndef __LIBPIN_DIE__UTIL__IVALMAP_HPP__
@@ -30,8 +30,8 @@
  *
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2013-03-07
- * @date      Last Update 2013-03-07
- * @version   0.1
+ * @date      Last Update 2013-03-08
+ * @version   0.2
  */
 template< typename KEY, typename VALUE >
 class IntervalMap
@@ -49,6 +49,7 @@ class IntervalMap
       Interval_s(const KEY& m, const VALUE& v) : max(m), value(v) {}
     } Interval;
     typedef std::map< KEY, Interval, std::greater< KEY > > Map;
+    typedef typename Map::iterator iterator;
   private: // Internal variables
     Map m_map; //!< An underlying map containing key/value pairs.
   public: // Inline generated methods
@@ -57,17 +58,29 @@ class IntervalMap
      *
      * @param key A key to be searched for.
      * @return An iterator to the element associated with the interval to which
-     *   the key belongs or Map::end if the key does not belong to any interval.
+     *   the key belongs or IntervalMap::end() if the key does not belong to any
+     *   interval in the map.
      */
-    typename Map::iterator find(const KEY& key)
+    iterator find(const KEY& key)
     {
       // Try to find the interval to which the key belongs
-      typename Map::iterator it = m_map.lower_bound(key);
+      iterator it = m_map.lower_bound(key);
 
       // Lower bound will give us the only interval which may contain the key
       if (it->first <= key && key < it->second.max) return it;
 
       // No interval found
+      return m_map.end();
+    }
+
+    /**
+     * Returns an iterator referring to the @em past-the-end element in the map
+     *   container.
+     *
+     * @return An iterator to the @em past-the-end element in the container.
+     */
+    iterator end()
+    {
       return m_map.end();
     }
 
@@ -78,8 +91,8 @@ class IntervalMap
      * @param max An upper bound of the interval.
      * @param value A value associated with the interval.
      */
-    std::pair< typename Map::iterator, bool > insert(const KEY& min,
-      const KEY& max, const VALUE& value)
+    std::pair< iterator, bool >
+    insert(const KEY& min, const KEY& max, const VALUE& value)
     {
       return m_map.insert(typename Map::value_type(min, Interval(max, value)));
     }
