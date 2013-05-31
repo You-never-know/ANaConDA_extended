b11a84bbbe796e7364b3ee70be18769d6543fb2d
Jan Fiedor <fiedorjan@centrum.cz>
2013-05-31 14:19:38 +0200

    framework: added a class simplifying management of thread local data.

---
+++ framework/src/utils/tldata.hpp
@@ -0,0 +1,103 @@
+/**
+ * @brief Contains a class simplifying management of thread local data.
+ *
+ * A file containing a class simplifying management of thread local data.
+ *
+ * @file      tldata.hpp
+ * @author    Jan Fiedor (fiedorjan@centrum.cz)
+ * @date      Created 2013-05-31
+ * @date      Last Update 2013-05-31
+ * @version   0.1
+ */
+
+#ifndef __PINTOOL_ANACONDA__UTILS__TLDATA_HPP__
+  #define __PINTOOL_ANACONDA__UTILS__TLDATA_HPP__
+
+#include "pin.H"
+
+/**
+ * @brief Simplifies management of thread local data.
+ *
+ * Encapsulates arbitrary data and provides access to them. Ensures automatic
+ *   initialisation and proper release of this data.
+ *
+ * @warning A thread may access local data of another thread. Note, however,
+ *   that the user must ensure proper synchronisation in this case to prevent
+ *   concurrency errors like data races.
+ *
+ * @tparam T A type of data the class should manage.
+ *
+ * @author    Jan Fiedor (fiedorjan@centrum.cz)
+ * @date      Created 2013-05-31
+ * @date      Last Update 2013-05-31
+ * @version   0.1
+ */
+template< typename T >
+class ThreadLocalData
+{
+  private: // Internal variables
+    /**
+     * @brief A key identifying the thread local storage (TLS) slot holding the
+     *   local data of a thread.
+     */
+    TLS_KEY m_tlsKey;
+  public: // Constructors
+    /**
+     * Constructs a ThreadLocalData object.
+     */
+    ThreadLocalData() : m_tlsKey(PIN_CreateThreadDataKey(free))
+    {
+      PIN_AddThreadStartFunction(init, &m_tlsKey);
+    }
+
+  public: // Destructors
+    /**
+     * Destroys a ThreadLocalData object.
+     */
+    ~ThreadLocalData()
+    {
+      PIN_DeleteThreadDataKey(m_tlsKey);
+    }
+
+  private: // Internal functions for initialising and releasing data
+    /**
+     * Initialises local data of a thread.
+     *
+     * @param tid A number identifying the thread.
+     * @param ctxt A structure containing the initial register state of the
+     *   thread.
+     * @param flags OS specific thread flags.
+     * @param v Arbitrary data passed to the callback registration function.
+     */
+    static VOID init(THREADID tid, CONTEXT* ctxt, INT32 flags, VOID* v)
+    {
+      PIN_SetThreadData(*static_cast< TLS_KEY* >(v), new T(), tid);
+    }
+
+    /**
+     * Frees local data of a thread.
+     *
+     * @param data A pointer to local data which should be freed.
+     */
+    static VOID free(VOID* data)
+    {
+      delete static_cast< T* >(data);
+    }
+
+  public: // Methods for accessing the data
+    /**
+     * Gets local data of a thread.
+     *
+     * @param tid A number identifying the thread.
+     * @return A pointer to local data of the thread.
+     */
+    inline
+    T* get(THREADID tid)
+    {
+      return static_cast< T* >(PIN_GetThreadData(m_tlsKey, tid));
+    }
+};
+
+#endif /* __PINTOOL_ANACONDA__UTILS__TLDATA_HPP__ */
+
+/** End of file tldata.hpp **/
