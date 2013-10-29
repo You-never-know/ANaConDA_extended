354e4302a73a2b988f37f920227a292abdb8f36c
Jan Fiedor <fiedorjan@centrum.cz>
2013-10-29 18:31:23 +0100

    tools/build: let GCC download and compile the required libraries for us.
    Removed variables used to specify paths to required libraries as the
    libraries are now automatically downloaded and compiled by GCC itself.

--- tools/build.sh
+++ tools/build.sh
@@ -5,7 +5,7 @@
 # Author:
 #   Jan Fiedor
 # Version:
-#   0.5.9
+#   0.5.10
 # Created:
 #   18.10.2013
 # Last Update:
@@ -304,21 +304,14 @@ build_gcc()
   print_info "     extracting... $GCC_STABLE_TGZ"
   tar xf ./$GCC_STABLE_TGZ
 
-  # Set paths to the required libraries
-  if [ -z "$GMP_HOME" ]; then
-    GMP_HOME=/usr
-  fi
-  if [ -z "$MPC_HOME" ]; then
-    MPC_HOME=/usr
-  fi
-  if [ -z "$MPFR_HOME" ]; then
-    MPFR_HOME=/usr
-  fi
-
   # Compile the source code
   print_info "     compiling... $GCC_STABLE_DIR"
   cd $GCC_STABLE_DIR
-  ./configure --enable-languages=c++,c --disable-bootstrap --disable-multilib --prefix=$INSTALL_DIR --with-gmp=$GMP_HOME --with-mpc=$MPC_HOME --with-mpfr=$MPFR_HOME && make && make -j1 install || terminate "cannot build GCC."
+  ./contrib/download_prerequisites
+  cd ..
+  mkdir -p gcc-build
+  cd gcc-build
+  ../$GCC_STABLE_DIR/configure --enable-languages=c++,c --disable-bootstrap --disable-multilib --prefix=$INSTALL_DIR && make && make -j1 install || terminate "cannot build GCC."
   cd ..
 
   # Update the environment
