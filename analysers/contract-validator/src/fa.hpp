d3d41657191eea07109546aa2c1f83c390be3f34
Jan Fiedor <fiedorjan@centrum.cz>
2014-11-28 16:38:58 +0100

    contract-validator: updated the implementation of the finite automaton.

--- analysers/contract-validator/src/fa.hpp
+++ analysers/contract-validator/src/fa.hpp
@@ -1,62 +1,87 @@
 /**
- * @brief Contains implementation of a class representing a finite state
- *   machine (FSM).
+ * @brief Contains implementation of a class representing a finite automaton.
  *
- * A file containing implementation of a class representing a finite state
- *   machine (FSM).
+ * A file containing implementation of a class representing a finite automaton.
  *
- * @file      fsm.hpp
+ * @file      fa.hpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2014-11-27
- * @date      Last Update 2014-11-27
- * @version   0.1
+ * @date      Last Update 2014-11-28
+ * @version   0.2
  */
 
-#ifndef __FSM_HPP__
-  #define __FSM_HPP__
-
-/**
- * @brief An enumeration of types of states which might be present in a finite
- *   state machine (FSM).
- */
-typedef enum FsmStateType_e
-{
-  NORMAL,    //!< A normal state.
-  STARTING,  //!< A starting state.
-  ACCEPTING, //!< An accepting state.
-} FsmStateType;
+#ifndef __FA_HPP__
+  #define __FA_HPP__
 
 /**
- * @brief A structure representing a state of a finite state machine (FSM).
+ * @brief A structure representing a state of a finite automaton (FA).
  */
-typedef struct FsmState_s
+typedef struct FAState_s
 {
   /**
-   * @brief A list of outgoing transitions.
+   * @brief A list of transitions that can be taken from the state.
    */
-  std::map< std::string, FsmState_s* > transitions;
-  FsmStateType type; //!< A type of the state.
-} FsmState;
+  std::map< std::string, FAState_s* > transitions;
+  bool accepting; //!< A flag determining if the state is accepting or not.
+
+  /**
+   * Constructs a new non-accepting state of a finite automaton (FA).
+   */
+  FAState_s() : accepting(false) {}
+
+  /**
+   * Constructs a new state of a finite automaton (FA).
+   *
+   * @param a A flag determining if the state is accepting or not. If set to @c
+   *   true, the state will be an accepting state. Otherwise, the state will be
+   *   a non-accepting state.
+   */
+  FAState_s(bool a) : accepting(a) {}
+} FAState;
 
 /**
- * @brief A class representing a finite state machine (FSM).
+ * @brief A structure representing a finite automaton (FA).
  *
- * Represents a finite state machine (FSM).
+ * @tparam S A class or structure representing states of the finite automaton.
+ */
+template< typename S >
+typedef struct FA_s
+{
+  /**
+   * @brief A class or structure representing states of the finite automaton.
+   */
+  typedef typename S State;
+
+  State* start; //!< A starting state.
+} FA;
+
+/**
+ * @brief A class representing a single run of a finite automaton (FA).
+ *
+ * Represents a single run of a finite automaton (FA).
  *
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
- * @date      Created 2014-11-27
- * @date      Last Update 2014-11-27
+ * @date      Created 2014-11-28
+ * @date      Last Update 2014-11-28
  * @version   0.1
  */
-class Fsm
+template< typename FA >
+class FARunner
 {
   private:
-    FsmState* m_start;
-    FsmState* m_current;
+    FA* m_fa; //!< The finite automaton whose run is described.
+    FA::State* m_current; //!< The current state of the finite automaton.
   public:
+    /**
+     * Advances the finite automaton to the next state.
+     *
+     * @param symbol A name of a symbol encountered in the execution.
+     * @return @em True if the finite automaton advanced to some next state,
+     *   @em false otherwise.
+     */
     bool advance(std::string symbol) { return false; }
 };
 
-#endif /* __FSM_HPP__ */
+#endif /* __FA_HPP__ */
 
-/** End of file fsm.hpp **/
+/** End of file fa.hpp **/
