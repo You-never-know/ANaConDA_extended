87f75bd097d88fdbfdf468ce899cb1b2e34f2077
Jan Fiedor <fiedorjan@centrum.cz>
2015-06-03 13:34:01 +0200

    tools: added a script for configuring and starting Cygwin on Windows.
    Cygwin is used to execute all of the scripts from the tools folders
    which allows one to easily build and run all parts of the ANaConDA
    framework.

---
+++ tools/cygwin.bat
@@ -0,0 +1,36 @@
+@rem
+@rem Description:
+@rem   A script for configuring and starting the Cygwin terminal so it is able
+@rem   to run the shell scripts in the tools folder.
+@rem Author:
+@rem   Jan Fiedor
+@rem Version:
+@rem   1.0
+@rem Created:
+@rem   03.06.2015
+@rem Last Update:
+@rem   03.06.2015
+@rem
+
+@if "%VS120COMNTOOLS%"=="" (
+  echo "error: Visual Studio 2013 not found."
+  exit /b 1
+)
+
+@call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+
+@for /f "tokens=1,2*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Cygwin\setup" /v "rootdir"') do (
+  @if "%%i"=="rootdir" (
+    @set "CYGWIN_HOME=%%k"
+  )
+)
+
+@if "%CYGWIN_HOME%"=="" (
+  echo "error: Cygwin not found."
+  exit /b 1
+)
+
+@rem Start a Cygwin bash terminal in the ANaConDA directory
+"%CYGWIN_HOME%\bin\mintty.exe" -i /Cygwin-Terminal.ico "%CYGWIN_HOME%\bin\bash.exe" -l -c "cd \"%~dp0/..\"; exec bash"
+
+@rem End of script
