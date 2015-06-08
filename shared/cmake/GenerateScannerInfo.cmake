c3d3ad4587d6bcc3a7516cd4e0651ae89fed0a48
Jan Fiedor <fiedorjan@centrum.cz>
2015-06-08 13:00:07 +0200

    shared/cmake: include builtin defs and special symbols in scanner info.
    When generating scanner information used by the Eclipse code analysis,
    include also builtin defines and special symbols. Special symbols are
    symbols recognised by the compiler, however, unknown to Eclipse.

--- shared/cmake/GenerateScannerInfo.cmake
+++ shared/cmake/GenerateScannerInfo.cmake
@@ -4,8 +4,8 @@
 # File:      GenerateScannerInfo.cmake
 # Author:    Jan Fiedor (fiedorjan@centrum.cz)
 # Date:      Created 2012-05-31
-# Date:      Last Update 2015-06-04
-# Version:   0.1.6
+# Date:      Last Update 2015-06-08
+# Version:   0.1.7
 #
 
 #
@@ -69,20 +69,61 @@ MACRO(GENERATE_SCANNER_INFO FILE)
   string(REGEX MATCHALL "[ ]/D[^ ]+" DEFINES ${FLAGS})
   string(REGEX MATCHALL "[ ]/I[^ ]+" INCLUDES ${FLAGS})
 
-  # Some (built-in) symbols are defined automatically by the compiler
-  set(BUILTIN_DEFINES "__int8=char" "__int16=short" "__int32=int" "__int64=long"
-    "__cplusplus=199711L" "__cdecl=__attribute__((__cdecl__))")
+  # ANSI-Compliant Predefined Macros
+  set(BUILTIN_C99_DEFINES "__STDC__")
+  # Microsoft-Specific Predefined Macros
+  set(BUILTIN_MS_DEFINES "_ATL_VER" "__AVX__" "__AVX2__" "_CHAR_UNSIGNED"
+    "__CLR_VER" "__cplusplus_cli" "__cplusplus_winrt" "__COUNTER__"
+    "__cplusplus" "_CPPRTTI" "_CPPUNWIND" "_DEBUG" "_DLL" "__FUNCDNAME__"
+    "__FUNCSIG__" "__FUNCTION__" "_INTEGRAL_MAX_BITS" "_M_AMD64" "_M_ARM"
+    "_M_CEE" "_M_CEE_PURE" "_M_CEE_SAFE" "_M_IX86" "_M_ARM_FP" "_M_IX86_FP"
+    "_M_X64" "_MANAGED" "_MFC_VER" "_MSC_BUILD" "_MSC_EXTENSIONS"
+    "_MSC_FULL_VER" "_MSC_VER" "__MSVC_RUNTIME_CHECKS" "_MT"
+    "_NATIVE_WCHAR_T_DEFINED" "_OPENMP" "_VC_NODEFAULTLIB" "_WCHAR_T_DEFINED"
+    "_WIN32" "_WIN64")
+  # All Predefined Macros
+  set(BUILTIN_DEFINES ${BUILTIN_C99_DEFINES} ${BUILTIN_MS_DEFINES})
 
-  # Have Visual Studio 2010 installed, assume that compiler version is 1600
-  if (DEFINED ENV{VS100COMNTOOLS})
-    set(BUILTIN_DEFINES ${BUILTIN_DEFINES} "_MSC_VER=1600")
-  endif (DEFINED ENV{VS100COMNTOOLS})
+  # Determine the source file used to retrieve the values of the defines
+  set(COMPILE_DIR "${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}")
+  set(SOURCE_FILE "${COMPILE_DIR}/compiler_defs.cpp")
 
-  # Export some built-in symbols so the code analyser in Eclipse knows them
+  # Helper macros which use preprocessor to print the values of the defines
+  file(WRITE "${SOURCE_FILE}"
+    "#define __STR2__(x) #x\n"
+    "#define __STR1__(x) __STR2__(x)\n"
+    "#define __PPOUT__(x) \"#define \" #x \" \" __STR1__(x)\n\n")
+
+  # This code prints the values of the specified defines
   foreach(DEFINE ${BUILTIN_DEFINES})
-    string(REPLACE "=" " " DEFINE ${DEFINE})
-    file(APPEND ${FILE} "#define ${DEFINE}\n")
+    file(APPEND "${SOURCE_FILE}"
+      "#if defined(${DEFINE})\n"
+      "  #pragma message(__PPOUT__(${DEFINE}))\n"
+      "#endif\n\n")
+  endforeach(DEFINE ${BUILTIN_DEFINES})
+
+  # Compile the file so the preprocessor would print the values of the defines
+  try_compile(RESULT "${COMPILE_DIR}" "${SOURCE_FILE}"
+    OUTPUT_VARIABLE OUTPUT)
+
+  # Extract the lines containing the values of the defines from the output
+  string(REGEX MATCHALL "#define[^\n]*\n" BUILTIN_DEFINES ${OUTPUT})
+
+  # Added the built-in defines so the code analyser in Eclipse knows them
+  foreach(DEFINE ${BUILTIN_DEFINES})
+    string(REPLACE "\n" "" DEFINE ${DEFINE})
+    file(APPEND ${FILE} "${DEFINE}")
   endforeach(DEFINE)
+
+  # Special symbols recognised by the compiler, but not defined anywhere
+  set(SPECIAL_SYMBOLS "__int8=char" "__int16=short" "__int32=int" "__int64=long"
+    "__cdecl=__attribute__((__cdecl__))")
+
+  # Include the special symbols so the code analyser in Eclipse knows them
+  foreach(SYMBOL ${SPECIAL_SYMBOLS})
+    string(REPLACE "=" " " SYMBOL ${SYMBOL})
+    file(APPEND ${FILE} "#define ${SYMBOL}\n")
+  endforeach(SYMBOL)
 
   # Defined symbols must be preceeded by #define and stored one per line
   foreach(ITEM ${DEFINES})
