82ca4affae57170b56b0eb9385f3cbc9c738ea96
Jan Fiedor <fiedorjan@centrum.cz>
2015-07-17 13:35:26 +0200

    framework/dll: added function for accessing hidden PIN framework handle.

--- framework/src/utils/windows/dll.cpp
+++ framework/src/utils/windows/dll.cpp
@@ -7,7 +7,7 @@
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2015-07-17
  * @date      Last Update 2015-07-17
- * @version   0.1
+ * @version   0.2
  */
 
 #include "dll.h"
@@ -18,13 +18,44 @@ extern "C" IMAGE_DOS_HEADER __ImageBase;
 #define CURRENT_MODULE_HANDLE ((HINSTANCE)&__ImageBase)
 
 /**
- * Gets a handle of the ANaConDA framework.
+ * Gets a handle of a module which contains a specific address.
  *
- * @return A handle of the ANaConDA framework.
+ * @param address An address belonging to the module.
+ * @return A handle of the module to which the specified address belongs.
  */
-HMODULE getAnacondaFrameworkHandle()
+HMODULE getModuleHandleByAddress(void* address)
+{
+  // Helper variables
+  MEMORY_BASIC_INFORMATION mbi;
+
+  // Get information about the memory of the module containing the address
+  if (VirtualQuery(address, &mbi, sizeof(mbi)) == 0) return 0;
+
+  // Allocation base is the base address of the module and also its handle
+  return reinterpret_cast< HMODULE >(mbi.AllocationBase);
+}
+
+/**
+ * Gets a handle of the hidden ANaConDA framework.
+ *
+ * @return A handle of the hidden ANaConDA framework.
+ */
+HMODULE getHiddenAnacondaFrameworkHandle()
 {
   return CURRENT_MODULE_HANDLE;
+}
+
+/**
+ * Gets a handle of the hidden PIN framework.
+ *
+ * @return A handle of the hidden PIN framework.
+ */
+HMODULE getHiddenPinFrameworkHandle()
+{
+  // This will get us the address of the GetProcAddress functions from the PIN
+  // framework instead of the kernel32.dll library as PIN replaces it with its
+  // own version so we will get the address of the PIN's version here
+  return getModuleHandleByAddress(&GetProcAddress);
 }
 
 /** End of file dll.cpp **/
--- framework/src/utils/windows/dll.h
+++ framework/src/utils/windows/dll.h
@@ -7,7 +7,7 @@
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2015-07-17
  * @date      Last Update 2015-07-17
- * @version   0.1
+ * @version   0.2
  */
 
 #ifndef __ANACONDA_FRAMEWORK__UTILS__WINDOWS__DLL_H__
@@ -15,7 +15,9 @@
 
 #include <windows.h>
 
-HMODULE getAnacondaFrameworkHandle();
+HMODULE getModuleHandleByAddress(void* address);
+HMODULE getHiddenAnacondaFrameworkHandle();
+HMODULE getHiddenPinFrameworkHandle();
 
 #endif /* __ANACONDA_FRAMEWORK__UTILS__WINDOWS__DLL_H__ */
 
