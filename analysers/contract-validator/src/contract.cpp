0bc92bca7589043170c737b0106b875b91c6f19a
Jan Fiedor <fiedorjan@centrum.cz>
2016-02-21 16:54:59 +0100

    contract-validator/contract: implemented basic regex to FA conversion.
    The constructed FA contains epsilon transitions.

--- analysers/contract-validator/src/contract.cpp
+++ analysers/contract-validator/src/contract.cpp
@@ -6,8 +6,8 @@
  * @file      contract.cpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2016-02-18
- * @date      Last Update 2016-02-19
- * @version   0.1
+ * @date      Last Update 2016-02-21
+ * @version   0.1.1
  */
 
 #include "contract.h"
@@ -22,6 +22,9 @@
 
 // Namespace aliases
 namespace fs = boost::filesystem;
+
+// Type definitions
+typedef boost::tokenizer< boost::char_separator< char > > Tokenizer;
 
 /**
  * Loads a contract from a file.
@@ -51,7 +54,6 @@ void Contract::load(const std::string& path)
     this->construct(mo[1].str());
 
     // If more spoilers can violate a target, they are separated by a comma
-    typedef boost::tokenizer< boost::char_separator< char > > Tokenizer;
     Tokenizer spoilers(mo[2].str(), boost::char_separator< char >(","));
 
     for (Tokenizer::iterator it = spoilers.begin(); it != spoilers.end(); ++it)
@@ -69,9 +71,70 @@ void Contract::load(const std::string& path)
  */
 FA* Contract::construct(const std::string& regex)
 {
-  std::cout << regex << "\n";
+  // Helper variables
+  FA::State* current = NULL;
+  FA::State* epsilon = NULL;
+  std::stack< FA::State* > states;
+  std::stack< FA::State* > epsilons;
 
-  return NULL;
+  // Create a new FA
+  FA* fa = new FA();
+  // Initialise the FA
+  fa->start = new FA::State();
+  fa->regex = regex;
+
+  // Here we go if we encounter an alternation in the regular expression
+  states.push(fa->start);
+
+  // The state we are currently in when performing the transformation
+  current = fa->start;
+
+  // Split the regular expression into parts, keep special characters
+  Tokenizer tokens(regex, boost::char_separator< char >(" ", "()|"));
+
+  for (Tokenizer::iterator it = tokens.begin(); it != tokens.end(); ++it)
+  { // Transform the regular expression into a finite automaton
+    switch ((*it).at(0))
+    { // Process the current token
+      case '(': // Start of a group
+        // Save the state marking the beginning of the current group
+        states.push(current);
+        // Save the epsilon state of the parent group
+        epsilons.push(epsilon);
+        epsilon = NULL;
+        break;
+      case ')': // End of a group
+        if (epsilon != NULL)
+        { // Redirect the end of a possible alternation to the epsilon state
+          current->transitions[""] = epsilon;
+          // Move to the end of the current group
+          current = epsilon;
+        }
+        // Load the state marking the beginning of the parent group
+        states.pop();
+        // Load the epsilon state of the parent group
+        epsilon = epsilons.top();
+        epsilons.pop();
+        break;
+      case '|': // Alternation
+        if (epsilon == NULL)
+        { // A state where all alternations end (by taking epsilon transition)
+          epsilon = new FA::State();
+        } // Redirect the end of a possible alternation to the epsilon state
+        current->transitions[""] = epsilon;
+        // Move back to the state marking beginning of the current group
+        current = states.top();
+        break;
+      default: // Name of a method
+        // Add a new transition for the encountered method in the current state
+        current->transitions[boost::trim_copy(*it)] = new FA::State();
+        // Advance to the new state by taking the newly added transition
+        current = current->transitions[boost::trim_copy(*it)];
+        break;
+    }
+  }
+
+  return fa;
 }
 
 /** End of file contract.cpp **/
