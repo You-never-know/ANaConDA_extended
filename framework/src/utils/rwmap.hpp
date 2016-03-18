1e429592a571e569b1afef9088ee1537686881b5
Jan Fiedor <fiedorjan@centrum.cz>
2016-03-18 15:45:05 +0100

    framework/rwmap: added implementation of UnsafeRWMap.
    This map allows (unsafe) updates meaning that the reference to a value
    obtained from the map may be invalidated if the value is updated. This
    map is usefull in situations where one can be sure that no thread is
    using a value currently being updated by other thread.

--- framework/src/utils/rwmap.hpp
+++ framework/src/utils/rwmap.hpp
@@ -15,7 +15,7 @@
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2012-06-02
  * @date      Last Update 2016-03-18
- * @version   0.2
+ * @version   0.3
  */
 
 #ifndef __PINTOOL_ANACONDA__UTILS__RWMAP_HPP__
@@ -26,6 +26,7 @@
 #include <map>
 
 #include "lockobj.hpp"
+#include "scopedlock.hpp"
 
 /**
  * @brief An concurrent R/W map which does not allow any updates.
@@ -114,6 +115,87 @@ class ImmutableRWMap : public RWLockableObject
 
       // Insert the key/value pair into the underlying map
       m_map.insert(typename Map::value_type(key, value));
+
+      // Key/value pair inserted, allow other threads to access the map
+      this->unlock();
+    }
+};
+
+/**
+ * @brief An concurrent R/W map which allows (unsafe) updates.
+ *
+ * An effective implementation of an R/W map which allows (unsafe) updates. The
+ *   map allows one to update existing values (or insert new values) and access
+ *   these values by a reference, however, the user itself must ensure that the
+ *   values are not updated when other threads are still using the old values!
+ *
+ * The access to the map is guarded by a read/write lock which ensures that all
+ *   operations are thread-safe. This lock ensures that nobody is searching for
+ *   values when they are being updated (or inserted). It does not check if any
+ *   thread is still using the old values using a reference returned before!
+ *
+ * @warning Obtained references to values in the map may be invalidated if the
+ *   values are updated! It is up to the developer to ensure that the values
+ *   are updated only when not used by the other threads!
+ *
+ * @tparam KEY A type of data representing mapped keys.
+ * @tparam VALUE A type of data representing mapped values.
+ *
+ * @author    Jan Fiedor (fiedorjan@centrum.cz)
+ * @date      Created 2016-03-18
+ * @date      Last Update 2016-03-18
+ * @version   0.1
+ */
+template< typename KEY, typename VALUE >
+class UnsafeRWMap : public RWLockableObject
+{
+  public: // Definition of underlying map
+    typedef std::map< KEY, VALUE > Map;
+  private: // Internal variables
+    Map m_map; //!< An underlying map containing key/value pairs.
+    VALUE m_defaultValue; //!< A default value returned when a key is not found.
+  public: // Constructors
+    UnsafeRWMap(const VALUE& dv) : m_defaultValue(dv) {}
+  public: // Inline generated methods
+    /**
+     * Gets a reference to a value associated with a specific key.
+     *
+     * @warning This reference may be invalidated when the value is updated!
+     *
+     * @param key A key to be searched for.
+     * @return The reference to the value associated with the specified key or
+     *   to the default value specified during object construction if the key
+     *   was not found.
+     */
+    const VALUE& get(const KEY& key)
+    { // Finding requires only read access to the underlying map
+      ScopedReadLock(this->m_lock);
+
+      // Check if the key is present in the underlying map
+      typename Map::iterator it = m_map.find(key);
+
+      if (it != m_map.end())
+      { // Key found, return the associated value
+        return it->second;
+      }
+
+      // Key not found, return the default value
+      return m_defaultValue;
+    }
+
+    /**
+     * Updates a specific key with a value associated with it. If the key is
+     *   not present in the map, it will be inserted.
+     *
+     * @param key A key to be updated.
+     * @param value A value associated with the key.
+     */
+    void update(const KEY& key, const VALUE& value)
+    { // Inserting a key/value pair must be exclusive
+      this->writelock();
+
+      // Insert or update the key/value pair in the underlying map
+      m_map[key] = value;
 
       // Key/value pair inserted, allow other threads to access the map
       this->unlock();
