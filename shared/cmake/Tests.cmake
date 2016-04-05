716d003cccb17d0402592c79f4bf859dc7fdb4d1
Jan Fiedor <fiedorjan@centrum.cz>
2016-04-05 18:45:23 +0200

    shared/cmake: test programs can be compiled using build-tests target.

--- shared/cmake/Tests.cmake
+++ shared/cmake/Tests.cmake
@@ -4,12 +4,40 @@
 # File:      Tests.cmake
 # Author:    Jan Fiedor (fiedorjan@centrum.cz)
 # Date:      Created 2016-03-24
-# Date:      Last Update 2016-04-01
-# Version:   0.1
+# Date:      Last Update 2016-04-05
+# Version:   0.2
 #
 
 # Enable commands for defining tests 
 enable_testing()
+
+# A target which compiles test programs
+add_custom_target(build-tests)
+
+#
+# Compiles a test program.
+#
+# COMPILE_TEST_PROGRAM(<test>)
+#
+macro(COMPILE_TEST_PROGRAM TEST)
+  # Collect source files of the program needed for the test
+  aux_source_directory(${TEST_DIR}/${TEST} TEST_SOURCES)
+
+  # Do not compile the test program when building the main program
+  add_executable(${TEST} EXCLUDE_FROM_ALL ${TEST_SOURCES})
+
+  # The name of the test may be in the <directory>/<test> format
+  get_filename_component(TEST_PROGRAM_DIR ${TEST} DIRECTORY)
+  get_filename_component(TEST_PROGRAM_NAME ${TEST} NAME)
+
+  # Store the test program's executable in the test folder
+  set_target_properties(${TEST} PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${TEST_DIR}/${TEST_PROGRAM_DIR}"
+    RUNTIME_OUTPUT_NAME "${TEST_PROGRAM_NAME}.test")
+
+  # Compile the test program when building test programs 
+  add_dependencies(build-tests ${TEST})
+endmacro(COMPILE_TEST_PROGRAM)
 
 #
 # Schedules a test which uses the ANaConDA framework.
@@ -17,8 +45,15 @@ enable_testing()
 # ADD_ANACONDA_TEST(<test>)
 #
 macro(ADD_ANACONDA_TEST TEST)
+  # Compile the program needed for the test
+  COMPILE_TEST_PROGRAM(${TEST})
+
   # Construct a command which performs the test
-  set(CMD "$ENV{ANACONDA_FRAMEWORK_HOME}/tools/run.sh" "--help")
+  set(CMD "$ENV{ANACONDA_FRAMEWORK_HOME}/tools/run.sh")
+
+  # Specify the analyser and program used for the test 
+  set(CMD ${CMD} "event-printer")
+  set(CMD ${CMD} "${TEST_DIR}/${TEST}.test")
 
   # Schedule the test to perform
   add_test(${TEST} ${CMD})
