eadf436db71f4737d79380eb46a631c573aa7f4e
Jan Fiedor <fiedorjan@centrum.cz>
2016-05-26 14:40:31 +0200

    tests/exceptions: added function calls around throws and catches.

--- tests/framework/monitoring/exceptions/exceptions.cpp
+++ tests/framework/monitoring/exceptions/exceptions.cpp
@@ -4,19 +4,38 @@
  * @file      exceptions.cpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2016-04-01
- * @date      Last Update 2016-04-01
- * @version   0.1
+ * @date      Last Update 2016-05-26
+ * @version   0.2
  */
 
 class TestException {};
+
+void before_throw()
+{
+}
+
+void after_throw()
+{
+}
+
+void in_catch()
+{
+}
 
 int main(int argc, char* argv[])
 {
   try
   {
+    before_throw();
+
     throw TestException();
+
+    after_throw();
   }
-  catch(...) {}
+  catch(...)
+  {
+    in_catch();
+  }
 }
 
 /** End of file exceptions.cpp **/
