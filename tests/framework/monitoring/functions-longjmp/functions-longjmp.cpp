5f755b8811f2ea0f4aa0a4434beb50d6092d206b
Jan Fiedor <fiedorjan@centrum.cz>
2016-06-08 15:22:06 +0200

    tests: testing recursive functions with longjump is now more robust.
    The updated test now uses information printed by the test program to
    check the correctness of the monitoring more precisely.

--- tests/framework/monitoring/functions-longjmp/functions-longjmp.cpp
+++ tests/framework/monitoring/functions-longjmp/functions-longjmp.cpp
@@ -4,33 +4,45 @@
  * @file      functions-longjmp.cpp
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2016-04-21
- * @date      Last Update 2016-04-21
- * @version   0.1
+ * @date      Last Update 2016-06-08
+ * @version   0.2
  */
 
 #include <setjmp.h>
+
+#include "../../../shared/defs.h"
 
 jmp_buf g_env;
 
 void recursive_test_function(int i)
 {
+  FUNCTION_START
+
   if (i != 0)
   {
     recursive_test_function(--i);
   }
   else
   {
+    FUNCTION_EXIT
+
     longjmp(g_env, 1);
   }
+
+  FUNCTION_EXIT
 }
 
 int main(int argc, char* argv[])
 {
+  FUNCTION_START
+
   int stop = 0;
 
   stop = setjmp(g_env);
 
   if (!stop) recursive_test_function(4);
+
+  FUNCTION_EXIT
 }
 
 /** End of file functions-longjmp.cpp **/
