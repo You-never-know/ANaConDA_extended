35b9243f62bd559cd984309b112436cea5cdce42
Jan Fiedor <fiedorjan@centrum.cz>
2016-07-26 11:03:09 +0200

    tests/shared: use __FUNCTION__ where __func__ is not supported.

--- tests/shared/defs.h
+++ tests/shared/defs.h
@@ -6,14 +6,18 @@
  * @file      defs.h
  * @author    Jan Fiedor (fiedorjan@centrum.cz)
  * @date      Created 2016-06-08
- * @date      Last Update 2016-06-08
- * @version   0.1
+ * @date      Last Update 2016-07-26
+ * @version   0.1.1
  */
 
 #ifndef __ANACONDA_TESTS__SHARED__DEFS_H__
   #define __ANACONDA_TESTS__SHARED__DEFS_H__
 
 #include <stdio.h>
+
+#ifndef __func__
+  #define __func__ __FUNCTION__
+#endif
 
 // Macros for flagging the beginning and the end of a function
 #define FUNCTION_START \
