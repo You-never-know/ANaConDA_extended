9d57f439f01cd6d7115e490cf15364b08b20d63c
Jan Fiedor <fiedorjan@centrum.cz>
2018-11-26 18:41:08 +0100

    tools: added license notice to all source files and scripts.

--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+#
+# Copyright (C) 2013-2018 Jan Fiedor <fiedorjan@centrum.cz>
+#
+# This file is part of ANaConDA.
+#
+# ANaConDA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# ANaConDA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ANaConDA. If not, see <http://www.gnu.org/licenses/>.
+#
+
 #
 # Description:
 #   A script simplifying building of the ANaConDA framework and its parts.
