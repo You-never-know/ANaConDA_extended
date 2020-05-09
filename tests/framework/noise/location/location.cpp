e9277e92efe0700b595f8705aa13475d6468cb7c
Jan Fiedor <fiedorjan@centrum.cz>
2020-05-09 22:46:33 +0200

    tests: added a new test checking correct injection of location noise.

---
+++ tests/framework/noise/location/location.cpp
@@ -0,0 +1,20 @@
+/**
+ * @brief Tests location noise.
+ *
+ * @file      location.cpp
+ * @author    Jan Fiedor (fiedorjan@centrum.cz)
+ * @date      Created 2020-04-12
+ * @date      Last Update 2020-04-12
+ * @version   0.1
+ */
+
+#include "../../../shared/defs.h"
+
+int main(int argc, char* argv[])
+{
+  FUNCTION_START
+
+  FUNCTION_EXIT
+}
+
+/** End of file location.cpp **/
